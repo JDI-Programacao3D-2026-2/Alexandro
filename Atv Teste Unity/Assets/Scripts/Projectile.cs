ec06362761996bf817642c81e851a9f25ff22d76
AlexYagam1 <114972137+AlexYagam1@users.noreply.github.com>
2026-09-25 21:40:49 -0300

    RECARGAS E INIMIGO ATIRANDO

--- Atv Teste Unity/Assets/Scripts/Projectile.cs
+++ Atv Teste Unity/Assets/Scripts/Projectile.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 20f;
     private Vector3 direction;
     private shootpool Shootpool;
 
+    void Awake()
+    {
+        Invoke("Deactivate", 5f); // Deactivate the projectile after 5 seconds if it doesn't hit anything
+    }
     public void StartProjectile(Vector3 dir, shootpool shooter)
     {
         this.direction = dir;
@@ -23,6 +27,12 @@ public class Projectile : MonoBehaviour
     {
         // Handle collision logic here (e.g., damage, effects, etc.)
         // After handling the collision, return the projectile to the pool
+        Shootpool.ReturnProjectile(gameObject);
+    }
+
+    private void Deactivate()
+    {
+        // Return the projectile to the pool if it hasn't hit anything
         Shootpool.ReturnProjectile(gameObject);
     }
 }
--- Atv Teste Unity/Assets/Scripts/shootpool.cs
+++ Atv Teste Unity/Assets/Scripts/shootpool.cs
@@ -1,20 +1,38 @@
 using UnityEngine;
 using UnityEngine.Pool;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class shootpool : MonoBehaviour
 {
+    [Header("Pool and Bullets Settings")]
     public GameObject projectilePrefab;
     public Transform SpawnBulletPoint;
     public int poolSize = 20;
     public int currentAmmo;
-    public int BulletShell = 3;
+    public int BulletShell;
+    public int maxBulletShell = 3;
     private int activeProjectiles = 0;
     private ObjectPool<GameObject> pool;
+
+    [Header("Shooting Settings")]
+    public bool isEnemy = false;
+    private bool canShoot = false;
+    private float fireRate = 0.5f;
+    private float nextFireTime = 0f;
+    public LayerMask layerMask;
+    public Transform playerTransform;
+    public Transform LauncherTransform;
+
+    //Criar categoria para objetos da ui
+    [Header("UI Elements")]
+    public TMPro.TextMeshProUGUI ammoText;
+    public TMPro.TextMeshProUGUI bulletShellText;
 
     void Awake()
     {
         currentAmmo = poolSize;
+        BulletShell = maxBulletShell;
 
         pool = new ObjectPool<GameObject>(
             createFunc: () => Instantiate(projectilePrefab),
@@ -25,20 +43,57 @@ public class shootpool : MonoBehaviour
             defaultCapacity: poolSize,
             maxSize: poolSize
         );
+
+        UpdateUI();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+    {   
+        //Comandos para o inimigo
+        if (isEnemy && canShoot && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+
+        if (isEnemy && currentAmmo <= 0 && BulletShell > 0 && IsInvoking("Reload"))
+        {
+            Invoke("Reload", 2f);
+            StartCoroutine(ReloadAnim());
+            //Se ficar sem pente precisa carregar por mais tempo
+            if (BulletShell <= 0)
+            {
+                canShoot = false;
+                StartCoroutine(EnemyReloadCoroutine());
+            }
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame && !isEnemy)
         {
             Shoot();
         }
 
         if (Keyboard.current.rKey.wasPressedThisFrame)
         {
-            currentAmmo = poolSize;
-            BulletShell --;
+            StartCoroutine(ReloadAnim());
+            Invoke("Reload", 2f);
+        }
+
+        UpdateUI();
+    }
+
+    void FixedUpdate()
+    {
+        canShoot = false;
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Mathf.Infinity, layerMask))
+        {
+            canShoot = true;
+            Debug.DrawLine(transform.position, hit.point, Color.green);
+
+        }else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.forward * 100f, Color.red);
         }
     }
 
@@ -59,11 +114,58 @@ public class shootpool : MonoBehaviour
         
     }
 
-    public void  ReturnProjectile(GameObject projectile)
+    public void ReturnProjectile(GameObject projectile)
     {
         activeProjectiles--;
         pool.Release(projectile);
     }
 
+    void UpdateUI()
+    {
+        if (isEnemy) return;
+        ammoText.text = "Balas: " + currentAmmo+"/" + poolSize;
+        bulletShellText.text = "Cartuchos: " + BulletShell + "/" + maxBulletShell;
+    }
 
+    private void Reload()
+    {
+        if (BulletShell > 0 && currentAmmo < poolSize)
+        {
+            BulletShell--;
+            currentAmmo = poolSize;
+            UpdateUI();
+        }
+    }
+
+    IEnumerator EnemyReloadCoroutine()
+    {
+        StartCoroutine(ReloadAnim());
+        yield return new WaitForSeconds(3f); // Tempo de recarga do inimigo
+        BulletShell = maxBulletShell;
+        currentAmmo = poolSize;
+
+    }
+
+    IEnumerator ReloadAnim()
+    {
+        float tempo = 0f;
+
+            // 1. Roda até 55 graus (em 1.5 segundos)
+            while (tempo < 1.5f)
+            {
+                tempo += Time.deltaTime;
+                float anguloX = Mathf.Lerp(0, 55, tempo / 0.5f);
+                LauncherTransform.localRotation = Quaternion.Euler(anguloX, 0, 0);
+                yield return null;
+            }
+            yield return new WaitForSeconds(0.5f); // Espera 0.5 segundos com o ângulo em 55 graus
+            tempo = 0f;
+            while (tempo < 2f)
+            {
+                tempo += Time.deltaTime;
+                float anguloX = Mathf.Lerp(55, 0, tempo / 1f);
+                LauncherTransform.localRotation = Quaternion.Euler(anguloX, 0, 0);
+                yield return null;
+            }
+    }
 }
